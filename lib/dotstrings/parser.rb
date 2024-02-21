9e85e32ff10e49fdeeec24e40dfedae9c1c38ec9
Ramon Torres <raymondjavaxx@gmail.com>
2024-02-21 12:45:05 -0500

    Prefer match? over match

--- lib/dotstrings/parser.rb
+++ lib/dotstrings/parser.rb
@@ -223,7 +223,7 @@ module DotStrings
     # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
     def parse_unicode(ch, &block)
-      raise_error("Unexpected character '#{ch}', expecting a hex digit") unless ch =~ TOK_HEX_DIGIT
+      raise_error("Unexpected character '#{ch}', expecting a hex digit") unless TOK_HEX_DIGIT.match?(ch)
 
       @unicode_buffer << ch
 
